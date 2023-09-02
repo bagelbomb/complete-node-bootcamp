32097536cdb9e14ef52bd4ee8b8eb7ff3155701d
bagelbomb <bagelbomb@gmail.com>
2023-09-01 20:07:58 -0700

    feat: checkpoint in section 14; implement CORS

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,13 +22,19 @@ const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
-app.enable('trust proxy');
-
+// Set views:
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files:
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable proxies (for Heroku):
+app.enable('trust proxy');
+
+// Implement CORS:
+app.use(cors());
+app.options('*', cors());
 
 // Set security HTTP headers:
 app.use(helmet());
